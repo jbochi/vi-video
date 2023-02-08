0d0f1e250d18732a613b1fc2b6ff793cb0b5c978
Juarez Bochi <juarez.bochi@shopify.com>
2023-02-08 16:02:20 -0500

    Don't pass filters if not speeding up or fading

--- src/vivideo/edit.py
+++ src/vivideo/edit.py
@@ -14,30 +14,20 @@ parser.add_argument("-media", "--input_media_file", help="Input file", required=
 parser.add_argument("-srt", "--input_srt_file", help="Input SRT file", required=True)
 parser.add_argument("-txt", "--input_txt_file", help="Edited transcription TXT file", required=True)
 parser.add_argument("-m", "--margin", default=50, help="Buffer between utterences", type=int)
-parser.add_argument("-f", "--fade", default=125, help="Fade In/Out Buffer between utterences", type=int)
+parser.add_argument("-f", "--fade_milliseconds", default=125, help="Fade In/Out Buffer between utterences", type=int)
 parser.add_argument("-s", "--speed", default=1.0, help="Playback speed", type=int)
 parser.add_argument("-o", "--output_file", default="output.mp4", help="Output media", type=str)
 
 # Docs: https://ffmpeg.org/ffmpeg.html
 parser.add_argument("-v", "--loglevel", help="FFMpeg loglevel option", default="16")
 
-# Python 3.9+ Supports parser.add_argument('--foo',
-#                           action=argparse.BooleanOptionalAction)
-parser.add_argument(
-    "-xf",
-    "--crossfade",
-    help="Media segments cross-fading",
-    dest="crossfade",
-    default=False,
-    action="store_true",
-)
 parser.add_argument(
     "-d",
     "--dry_run",
     help="Dry run",
     dest="dry_run",
     default=False,
-    action="store_true",
+    action=argparse.BooleanOptionalAction,
 )
 parser.add_argument(
     "-g",
@@ -45,8 +35,7 @@ parser.add_argument(
     help="Greedily selects next word, so runs faster",
     dest="greedy",
     default=True,
-    action="store_true"
-    # action=argparse.BooleanOptionalAction,
+    action=argparse.BooleanOptionalAction,
 )
 
 
@@ -77,51 +66,62 @@ def media_edit():
         desired_transcription = f.read()
 
     margin = datetime.timedelta(milliseconds=args.margin)
-    crossfade = datetime.timedelta(milliseconds=args.fade if args.crossfade else 0)
 
     output_path = Path(args.output_file)
     segments = []
     cmds = []
     cuts = list_cuts(subtitles, desired_transcription=desired_transcription, margin=margin)
+    fade_seconds = datetime.timedelta(milliseconds=args.fade_milliseconds).total_seconds()
+    audio_video_filter = []
 
     for c, cut in enumerate(cuts):
         segment_path = output_path.with_stem(f"segment_{c}")
 
+        if args.fade_milliseconds > 0:
+            audio_video_filter = [
+                # video Filter
+                "-vf",
+                ",".join(
+                    [
+                        f"fade=t=in:st={cut.start.total_seconds()}:d={fade_seconds}",
+                        f"fade=t=out:st={cut.end.total_seconds()-fade_seconds}:d={fade_seconds}",
+                        "setpts=N/FRAME_RATE/TB",
+                    ]
+                ),
+                # # Audio Filter
+                "-af",
+                ",".join(
+                    [
+                        f"afade=t=in:st={cut.start.total_seconds()}:d={fade_seconds}",
+                        f"afade=t=out:st={cut.end.total_seconds()-fade_seconds}:d={fade_seconds}",
+                        "asetpts=N/SR/TB",
+                    ]
+                ),
+            ]
+
         # Create List of Segment Trim w/ Fade Commands for FFMpeg
-        cmd = [
-            "ffmpeg",
-            # Input media file
-            "-i",
-            args.input_media_file,
-            # Trim
-            "-ss",
-            f"{cut.start.total_seconds()}",
-            "-to",
-            f"{cut.end.total_seconds()}",
-            # video Filter
-            "-vf",
-            ",".join(
-                [
-                    f"fade=t=in:st={cut.start.total_seconds()}:d={crossfade.total_seconds()}",
-                    f"fade=t=out:st={cut.end.total_seconds()-crossfade.total_seconds()}:d={crossfade.total_seconds()}",
-                    "setpts=N/FRAME_RATE/TB",
-                ]
-            ),
-            # Audio Filter
-            "-af",
-            ",".join(
-                [
-                    f"afade=t=in:st={cut.start.total_seconds()}:d={crossfade.total_seconds()}",
-                    f"afade=t=out:st={cut.end.total_seconds()-crossfade.total_seconds()}:d={crossfade.total_seconds()}",
-                    "asetpts=N/SR/TB",
-                ]
-            ),
-            # Overwrite output, Loglevels
-            "-y",
-            "-v",
-            args.loglevel,
-            str(segment_path),
-        ]
+        cmd = (
+            [
+                "ffmpeg",
+                # Input media file
+                "-i",
+                args.input_media_file,
+                # Trim
+                "-ss",
+                f"{int(1000 * cut.start.total_seconds())}ms",
+                "-to",
+                f"{int(1000 * cut.end.total_seconds())}ms",
+            ]
+            + audio_video_filter
+            + [
+                # Overwrite output
+                "-y",
+                # Loglevel
+                "-v",
+                args.loglevel,
+                str(segment_path),
+            ]
+        )
         cmds.append(cmd)
 
         # Create List of Segment Files Filter Options for FFMpeg Concat
@@ -134,34 +134,42 @@ def media_edit():
             print("Running command: %i" % i, " ".join(cmd))
             subprocess.run(cmd)
 
+    if args.speed != 1.0:
+        speed_args = [
+            "-filter_complex",
+            f"[0:v]setpts={1.0/args.speed}*PTS[v];[0:a]atempo={args.speed}[a]",
+            "-map",
+            "[v]",
+            "-map",
+            "[a]",
+        ]
+    else:
+        speed_args = []
+
     # Concat Segments
     list_path = output_path.with_stem("video_segments").with_suffix(".txt")
-    cmd_concat = [
-        "ffmpeg",
-        # Force Format
-        "-f",
-        "concat",
-        "-safe",
-        "0",
-        # File of input media
-        "-i",
-        f"{str(list_path)}",
-        # Speed Up/Down
-        "-filter_complex",
-        f"[0:v]setpts={1.0/args.speed}*PTS[v];[0:a]atempo={args.speed}[a]",
-        "-map",
-        "[v]",
-        "-map",
-        "[a]",
-        # Stream Copy
-        # '-c',
-        # 'copy',
-        # Overwrite output, Loglevels
-        "-y",
-        "-v",
-        args.loglevel,
-        str(output_path),
-    ]
+    cmd_concat = (
+        [
+            "ffmpeg",
+            # Force Format
+            "-f",
+            "concat",
+            "-safe",
+            "0",
+            # File of input media
+            "-i",
+            f"{str(list_path)}",
+        ]
+        + speed_args
+        + [
+            # Overwrite output
+            "-y",
+            # Loglevel
+            "-v",
+            args.loglevel,
+            str(output_path),
+        ]
+    )
 
     if args.dry_run:
         print(" ".join(cmd_concat))
