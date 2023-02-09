1226fb9a039435df7766d971fc821fd08ce8ca4e
Moe Maysami <moe.maysami@shopify.com>
2023-02-09 16:47:56 -0500

    Add video concat shell script

--- script/concat.sh
+++ script/concat.sh
@@ -1,10 +1,13 @@
 #!/bin/zsh
-# $1:source1 $3:destination
-usage="Usage: concat <input list> <output media> \n"
-usage+=" ffmpeg -i <input list> -c:v copy -c:a aac  <output media> \n"
-usage+=" <input list> Line format: file  <name.ext>"
+# $1:source1 $2:source12 $3:destination
+usage="Usage: concat <input media1> <input media2> <output media> \n"
+usage+=' ffmpeg -i "$1" -i "$2" -filter_complex "[0:v] [0:a] [1:v] [1:a] concat=n=2:v=1:a=1 [v] [a]" -map "[v]" -map "[a]" "$3"'
 [[ -z $1 || $1 = -h || $1 = --help ]] &&
 echo $usage && return 0
 [[ ! -f $1 ]] &&
 echo -e "$usage\nSource video not a file: '$1'" && return 1
-ffmpeg -f concat -safe 0 -i "$1" -c:v copy -c:a aac "$2"
+[[ ! -f $2 ]] &&
+echo -e "$usage\nSource video not a file: '$2'" && return 1
+
+# ffmpeg -f concat -i "$1" -c:v copy -c:a aac "$2" (Can misalign audio/video)
+ffmpeg -i "$1" -i "$2" -filter_complex "[0:v] [0:a] [1:v] [1:a] concat=n=2:v=1:a=1 [v] [a]" -map "[v]" -map "[a]" "$3"
